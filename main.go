54adfdcc001b8453bf6fa57c525fdeaf0d986469
Andrew <asoborev@mail.ru>
2025-11-20 20:32:06 +0300

    работа с конструкцией switch

--- main.go
+++ main.go
@@ -1,32 +1,42 @@
 package main
 
-import (
-	"errors"
-	"fmt"
+import "fmt"
+
+type Day int
+
+const (
+	Monday Day = iota
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+	Sunday
 )
 
-/*
-	для обработки ошибки используется пакет errors
-	мы можем передать как ошибку errors.New() так и отсутсвие ошибки nil
-	далее можно проверить если не ошибка то вывести сообщение, если ошибка вывести ошибку
-*/
-
 func main() {
-	information, err := getPass(20)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	plan := getPlans(Sunday)
 
-	fmt.Println(information)
+	fmt.Println(plan)
 }
 
-func getPass(age int) (string, error) {
-	if age >= 18 && age < 50 {
-		return "Проходите", nil
-	} else if age > 50 {
-		return "Поберегите себя", errors.New("you are too old")
+func getPlans(dayOfWeek Day) string {
+	switch dayOfWeek {
+	case Monday:
+		return "Отличный день что бы начать то что давно откладывал"
+	case Tuesday:
+		return "Запланирую тренировку"
+	case Wednesday:
+		return "Подготовить доклад"
+	case Thursday:
+		return "Разобрать новую тему по go"
+	case Friday:
+		return "Немного отдохнем"
+	case Saturday:
+		return "Поездака за город"
+	case Sunday:
+		return "Запланируй следующюю неделю"
+	default:
+		return "не валидный день недели"
 	}
-
-	return "Вам еще нет 18", errors.New("you are too young")
 }
