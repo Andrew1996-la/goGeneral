e72fe240f7f8c28ca87b2eeb3ffa86b4aca474f8
Andrew <asoborev@mail.ru>
2025-11-24 17:43:08 +0300

    matrix прямая и обратная диогональ

--- main.go
+++ main.go
@@ -3,12 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	//var mySlice []string // <- при такой инициализации слайса его указатель nil
-	mySlice := make([]string, 50)
+	getDiogonalMatix(10, 10)
+	getReverseDiogonalMatix(10, 10)
+}
 
-	mySlice = append(mySlice, "51")
+func getDiogonalMatix(axisX int, axisY int) {
+	matrix := make([][]int, axisX)
 
-	fmt.Println(mySlice)
-	fmt.Println(len(mySlice))
-	fmt.Println(cap(mySlice))
+	for x := 0; x < axisX; x++ {
+		matrix[x] = make([]int, axisY)
+
+		for y := 0; y < axisY; y++ {
+			if x == y {
+				matrix[x][y] = y
+			}
+		}
+
+		fmt.Println(matrix[x])
+	}
+}
+
+func getReverseDiogonalMatix(axisX int, axisY int) {
+	matrix := make([][]int, axisX)
+
+	for x := 0; x < axisX; x++ {
+		matrix[x] = make([]int, axisY)
+
+		for y := axisY - 1; y > 0; y-- {
+			if x + y == axisY - 1{
+				matrix[x][y] = y
+			}
+		}
+
+		fmt.Println(matrix[x])
+	}
+
 }
