a36cf148c1ce6341fd3e328ca6888e18d71b3f90
Andrew <asoborev@mail.ru>
2025-11-26 01:51:55 +0300

    value reciver & pointer reciver

--- main.go
+++ main.go
@@ -6,68 +6,44 @@ import "fmt"
 СТРУКТУРЫ
 */
 
-/*
-Часто структуры задаются именно таким образом
-для удобства переиспользования
-*/
-type Car struct {
-	manufacturer string
-	cost         int
-	color        string
-	power        int
-	hasLeftWeel  bool
+type User struct {
+	id      int
+	name    string
+	married bool
+}
+
+func NewUser(id int, name string, married bool) User {
+	return User{
+		id:      id,
+		name:    name,
+		married: married,
+	}
 }
 
 /*
-функция конструктор
+Метод value reciver. Означает что метод принимает копию User и не может ее менять.
+фактически выделяет новую ячейку памяти для User.
 */
-func NewCar(manufacturer, color string, cost, power int, hasLeftWeel bool) Car {
-	return Car{
-		manufacturer: manufacturer,
-		color:        color,
-		cost:         cost,
-		power:        power,
-		hasLeftWeel:  hasLeftWeel,
-	}
+func (u User) printUser(name string) {
+	u.name = name
+	fmt.Println(u.id, u.name, u.married)
+}
+
+/*
+Метод pointer reciver. Означает что метод принимает указатель на оригинального User
+и способен его модифицировать
+*/
+func (u *User) changeUser(name string) {
+	u.name = name
+	fmt.Println(u.id, u.name, u.married)
 }
 
 func main() {
+	//user1 := NewUser(1, "John", false) // <- оригинал
+	//user1.printUser("Peter")           // <- пробуем модифицировать
+	//fmt.Println(user1)                 // <- остается все как было
 
-	/*
-		один из способов инициализцации структуры.
-		инициализируктся вместе со значением, не переиспользуется
-	*/
-	// car := struct {
-	// 	manufacturer string
-	// 	cost         int
-	// 	color        string
-	// 	power        int
-	// 	hasLeftWeel  bool
-	// }{
-	// 	manufacturer: "BMW",
-	// 	cost:         2000000,
-	// 	color:        "Black",
-	// 	power:        350,
-	// 	hasLeftWeel:  true,
-	// }
-
-	/*
-		инициализация структуры отнаследоавнной от Car
-	*/
-	car1 := Car{
-		manufacturer: "Volvo",
-		cost:         1500000,
-		color:        "white",
-		power:        180,
-		hasLeftWeel:  true,
-	}
-
-	car2 := NewCar("Lada", "black", 250000, 80, true) // <- инициализация через конструктор
-	
-	
-
-	fmt.Println(car1.cost) // <- обращение к конкретному свойству структуры по ключу
-	fmt.Println(car1)
-	fmt.Println(car2)
-
+	user2 := NewUser(2, "Ann", true) // <- оригинал
+	user2.changeUser("Jula")         // <- модифицируем
+	fmt.Println(user2)               // <- модифицированный user так как был передан pointer reciver
 }
